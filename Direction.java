78e90c98d97a5f5fbf4bc9be70bb0fd6956a6714
marcushio <marcus.t.trujillo@gmail.com>
2017-12-04 17:50:56 -0700

    Direction enums have been made

--- Direction.java
+++ Direction.java
@@ -5,7 +5,22 @@
  * @author Marcus Trujillo
  * @version 12.2.17
  */
-public class Direction
+public enum Direction
 {
+    NORTH("north"), SOUTH("south"), EAST("east"), WEST("west"); 
     
+    String direction; 
+    /**
+     * Direction constructor sets the corresponding string direction to the direction. 
+     */
+    Direction(String direction){
+        this.direction = direction; 
+    }
+    
+    /**
+     * @return direction the string form of the direction
+     */
+    public String toString(){
+        return direction; 
+    }
 }
--- GameObject.java
+++ GameObject.java
@@ -1,11 +1,50 @@
 
 /**
- * Description of class GameObject here.
+ * The GameObject is an everyday object that exist in the world of the game. Ids are used to refer to 
+ * unique individual GameObjects even if you have multiple objects with the same name. 
+ * Players can use the "interact" command to interact and use the objects. 
  *
  * @author Marcus Trujillo
  * @version 12.2.17
  */
-public class GameObject
-{
+public abstract class GameObject
+{   
+    int id; 
+    String name; 
+    String objectDescription; 
+    String interactDescription; 
     
+    /**
+     * The GameObject constructor that sets the id, name, and descriptions for both the object itself and
+     * what happens when you interact with the game object. 
+     */
+    public GameObject(String objectDescription, String interactDescription, String name){
+        this.objectDescription = objectDescription; 
+        this.interactDescription = interactDescription; 
+        this.name = name; 
+    }
+    /**
+     * @return interactDescription 
+     */
+    public String getInteractDescription(){
+        return interactDescription; 
+    }
+    /**
+     * @return objectDescription a physical description of the object itself. 
+     */
+    public String getObjectDescription(){
+        return objectDescription; 
+    }
+    /**
+     *  This is the action when the player interacts with the GameObject. 
+     */
+    public void interact(){
+        
+    }
+    /**
+     * Returns the id of the object
+     */
+    public int getId(){
+        return id; 
+    }        
 }
