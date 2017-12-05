db5f6b3ee40af1f2fd820974ee923797b4b7f27d
Colton Trujillo <this.colton@gmail.com>
2017-12-04 17:36:19 -0700

    Documented and implemented rudimentary code for GameModel.

--- GameModel.java
+++ GameModel.java
@@ -1,11 +1,35 @@
-
+import java.util.Collection;
 /**
  * Description of class GameModel here.
  *
- * @author Marcus Trujillo
- * @version 12.2.17
+ * @author Colton Trujillo
+ * @version 12.4.17
  */
 public class GameModel
 {
-    
+    String message;
+    Collection<Location> locations;
+    Collection<Person> people;
+    LocationFactory locationFactory;
+    /**
+     * Sets up initial conditions for the game. This includes creating all locations, putting objects in them, and giving them exits. It also includes creating people and giving
+     * them initial locations. 
+     */
+    public void initialize(){
+        
+    }
+    /**
+     * Updates this GameModel's message. 
+     */
+    public void updateMessage(){
+        
+    }
+    /**
+     * Returns the current message of this GameModel.
+     * 
+     * @return String message to be displayed to the user reflecting the current state of the game.
+     */
+    public String getMessage(){
+        return null;
+    }
 }
