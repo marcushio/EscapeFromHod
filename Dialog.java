1d8dc06c9e2ac27fd332075001479f6471d6ceb8
Colton Trujillo <this.colton@gmail.com>
2017-12-05 15:30:16 -0700

    Turned Dialog into an enumeration and added many constants to hold the dialog that will be displayed for the game.

--- Dialog.java
+++ Dialog.java
@@ -5,7 +5,21 @@
  * @author Marcus Trujillo
  * @version 12.2.17 
  */
-public class Dialog
+public enum Dialog
 {
-    
+    WELCOME("Welcome"), 
+    TEST("Test"),
+    LIVING_ROOM_DESCRIPTION("You are in the living room."),
+    DINING_ROOM_DESCRIPTION("You are in the dining room."),
+    CLOSET_DESCRIPTION("You are in the closet."),
+    KITCHEN_DESCRIPTION("You are in the kitchen."),
+    LAUNDRYROOM_DESCRIPTION("You are in the laundry room."),
+    GARAGE_DESCRIPTION("You are in the garage.");
+    String text;
+    Dialog(String text){
+        this.text = text;
+    }
+    public String toString(){
+        return text;
+    }
 }
