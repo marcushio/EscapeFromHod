9b0da705768dbf9a7f3f771d36d77ea9eada126e
Colton Trujillo <this.colton@gmail.com>
2017-12-04 17:58:48 -0700

    Added new implementation code to LocationFactory.

--- LocationFactory.java
+++ LocationFactory.java
@@ -1,4 +1,4 @@
-
+import java.util.ArrayList;
 /**
  * Class to create Locations. 
  *
@@ -10,5 +10,22 @@ public class LocationFactory
    public LocationFactory(){
        
     }
-   
+   public ArrayList<Location> getLocations(){
+       return null;
+    }
+    private ArrayList<Location> makeLocations(){
+        ArrayList <Location> locations = new ArrayList<>();
+        
+        Location livingRoom = new Location("You are in the living room.");
+        
+        Location diningRoom = new Location("You are in the dining room.");
+        Location closet = new Location ("You are in the closet.");
+        Location kitchen = new Location("You are in the kitchen.");
+        Location laundryRoom = new Location("You are in the laundry room.");
+        Location garage = new Location("You are in the garage.");
+        
+        //adding exits
+        //livingRoom.addExit(diningRoom, Direction.)
+        return locations;
+    }
 }
