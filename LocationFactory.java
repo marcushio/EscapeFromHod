89f1c983787373f829584e969f485761bdeb260a
Colton Trujillo <this.colton@gmail.com>
2017-12-04 23:36:45 -0700

    Partially mplemented LocationFactory code to create locations.

--- LocationFactory.java
+++ LocationFactory.java
@@ -7,25 +7,35 @@ import java.util.ArrayList;
  */
 public class LocationFactory
 {
+    
    public LocationFactory(){
        
     }
+    /*
+     * Returns an ArrayList with all of the game's Locations. 
+     * 
+     * @return ArrayList<Location> containing Location objects representing all the places a player can go in the game.
+     */
    public ArrayList<Location> getLocations(){
-       return null;
+       return makeLocations();
     }
     private ArrayList<Location> makeLocations(){
         ArrayList <Location> locations = new ArrayList<>();
-        
         Location livingRoom = new Location("You are in the living room.");
-        
         Location diningRoom = new Location("You are in the dining room.");
         Location closet = new Location ("You are in the closet.");
         Location kitchen = new Location("You are in the kitchen.");
         Location laundryRoom = new Location("You are in the laundry room.");
         Location garage = new Location("You are in the garage.");
-        
+        //creating and adding exits
+        Exit lrEast = new Exit(diningRoom, "A sinister archway into another room.");
+        livingRoom.addExit(lrEast, Direction.EAST);
+        Exit drSouth = new Exit(kitchen, "An open entrance to a spooky kitchen");
+        kitchen.addExit(drSouth, Direction.SOUTH);
+        Exit kSouth = new Exit(laundryRoom, "An unassuming door.");
+        kitchen.addExit(kSouth, Direction.SOUTH);
         //adding exits
-        //livingRoom.addExit(diningRoom, Direction.)
+    
         return locations;
     }
 }
