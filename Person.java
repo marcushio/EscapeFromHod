1b9975b33516bc24af8db116f2d73259b7734710
marcushio <marcus.t.trujillo@gmail.com>
2017-12-05 17:56:39 -0700

    person constructor has been made

--- Person.java
+++ Person.java
@@ -9,14 +9,19 @@ public class Person
 {   
     Location location; 
     GameObject object; 
-    
-    
-    
+    String name; 
+    /**
+     * Constructor for person. Gives person a name. Client code is to use the setLocation in order to place person. 
+     */
+    public Person(String name){
+        this.name = name; 
+    }
+   
     /**
      * Sets the location of the person object in the world. 
      */
     void setLocation(Location location){
-        
+        this.location = location; 
     }
     
     /**
@@ -24,6 +29,13 @@ public class Person
      */
     Location getLocation(){
         return location; 
+    }
+    
+    /**
+     * The action for the person object to interact with a GameObject. The results will depend on the particular object the person interacts with 
+     */
+    public void interact(GameObject object){
+        object.interact(); 
     }
     
 }
