3bccf00b8887d7a5bfe55b74ce0e30fbd596528b
Colton Trujillo <this.colton@gmail.com>
2017-12-06 17:19:30 -0700

    Updated Controller to actually change the location of the player based on the input.

--- Controller.java
+++ Controller.java
@@ -12,7 +12,32 @@ public class Controller
        this.model = model;
     }
    public void takeInput(String input){
+       String[] tokens = input.split(" ");
+       switch(tokens[0]){
+           case "go": go(tokens[1]);
+           case "use": use(tokens[1]);
+           
+        }
        model.updateMessage();
-       model.changeCurrentLocation(Direction.EAST);
     }
+    
+    private void go(String input){
+       
+        switch(input){
+            case "north": model.changeCurrentLocation(Direction.NORTH);
+            break;
+            case "south": model.changeCurrentLocation(Direction.SOUTH);
+            break;
+            case "east": model.changeCurrentLocation(Direction.EAST);
+            break;
+            case "west": model.changeCurrentLocation(Direction.WEST);
+            default:
+        }
+        
+    }
+    private void use(String input){
+        
+    }
+    
+  
 }
