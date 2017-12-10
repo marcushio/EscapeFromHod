5f880c3b8d1a7e239fac7193e03b29a3af4e16c2
marcushio <marcus.t.trujillo@gmail.com>
2017-12-10 11:50:31 -0700

    Added documentations
    added to the javadocs

--- FullCommands.java
+++ FullCommands.java
@@ -11,33 +11,36 @@ public class FullCommands
     public String secondWord; 
     
     /**
-     * 
+     * create a full command. Give a first (Command) word and a second. 
+     * @param command. If it's not recognized it'll be the UNKNOWN Command.
      */
     public FullCommands(Command command, String secondWord){
         this.command = command; 
         this.secondWord = secondWord; 
     }
     /**
-     * 
+     * return the Command part of the FullCommand. 
+     * @return command
      */
     public Command getCommand(){
         return command; 
     }
     
     /**
-     * 
+     *
+     * @return the second word. It returns null if there wasn't a second. 
      */
     public String getSecondWord(){
         return secondWord; 
     }
     /**
-     * 
+     * @return true if this command wasn't understood. 
      */
     public boolean isUnknown(){
         return (command == Command.UNKNOWN); 
     }
     /**
-     * 
+     * @return true if the command has a second word. 
      */
     public boolean hasSecondWord(){
         return (secondWord != null); 
